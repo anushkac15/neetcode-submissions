24dd71f4402f4cc0172888afbe572791f95d6256
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-13 19:55:24 +0530

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        def solve(openP, closeP, res, temp):
+
+            if openP + closeP == 2 * n:
+                res.append(temp[:])
+                return
+
+            if openP < n:
+                solve(openP + 1, closeP, res, temp + "(")
+            if closeP < openP:
+                solve(openP, closeP + 1, res, temp + ")")
+
+        res = []
+        solve(0, 0, res, "")
+        return res

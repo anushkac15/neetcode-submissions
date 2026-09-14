f39881e878d4b22c8bb586337d46cabab3371081
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-14 13:46:03 +0530

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        def solve(i, dp):
+
+            if i>=len(cost):
+                return 0
+
+            if dp[i] !=-1:
+                return dp[i]
+
+            one = solve(i+1, dp)+cost[i]
+            two = solve(i+2, dp) + cost[i]
+
+            dp[i] = min(one, two)
+            return dp[i]
+
+        dp = [-1]*len(cost)
+        return min(solve(0,dp), solve(1,dp))
+
+
+        

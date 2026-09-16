3d9a2a60e1bdef2a5692a52ad63696e31e6aa235
Anushka Gupta <119026498+anushkac15@users.noreply.github.com>
2026-09-16 13:17:57 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        
+
+        def solve(i, j, dp):
+
+            if j<0:
+                return i+1
+            if i<0:
+                return j+1
+            
+            if dp[i][j] !=-1:
+                return dp[i][j]
+
+            if word1[i] == word2[j]:
+                dp[i][j] = solve(i-1, j-1, dp)
+                return dp[i][j]
+
+            insert = solve(i,j-1, dp)
+            delete = solve(i-1, j, dp)
+            replace = solve(i-1, j-1, dp)
+
+            dp[i][j] = 1+min(insert, min(replace, delete))
+
+            return dp[i][j]
+
+        dp = [[-1]*len(word2) for _ in range(len(word1))]
+        return solve(len(word1)-1, len(word2)-1, dp)
+
+
+
+            
